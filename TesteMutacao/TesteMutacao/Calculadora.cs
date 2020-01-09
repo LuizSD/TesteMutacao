d5f08d3c292fd0cd466ab9780b4ccd5d798b8aa8
Luiz Santos <luiz.santos@ditdigital.com.br>
2020-01-09 16:29:37 -0300

    Criando calculadora

---
+++ TesteMutacao/TesteMutacao/Calculadora.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TesteMutacao
+{
+    public class Calculadora
+    {
+        public int Soma(int primeiroNumero, int segundoNumero) {
+
+            if (segundoNumero != default)
+                primeiroNumero += segundoNumero;
+
+            return primeiroNumero;
+        }
+
+    }
+}
